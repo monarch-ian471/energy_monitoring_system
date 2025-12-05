77a6b8304d96587811d62ca05c17a44f5d014499
monarch-ian471 <iankatengeza@gmail.com>
2025-12-05 05:48:40 +0200

    Api_fix

--- flutter_app/lib/src/core/constants.dart
+++ flutter_app/lib/src/core/constants.dart
@@ -1,4 +1,1 @@
-const bool useDummyData = false;
-const String apiBaseUrl = useDummyData
-    ? 'https://6208df89-04bf-456d-bcf3-a2cac4c6dd00.mock.pstmn.io'
-    : 'http://192.168.1.140:8000';
+const String apiBaseUrl = 'http://192.168.1.140:8000';
